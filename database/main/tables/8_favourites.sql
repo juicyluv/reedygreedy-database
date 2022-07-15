6710642257d3ccc151bb5865eb54de5a8f56f419
juicyluv <juicyluv@mail.ru>
2022-07-15 23:18:39 +0300

    issue #32 - Create favourites table

---
+++ database/main/tables/8_favourites.sql
@@ -0,0 +1,6 @@
+create table if not exists main.favourites(
+  user_id bigint not null references main.users(id),
+  book_id bigint not null references main.books(id)
+);
+
+alter table main.favourites owner to postgres;
