67d722d6a85175f7368ee1273ace910c5821a823
Ilya <57060144+juicyluv@users.noreply.github.com>
2022-07-15 23:08:55 +0300

    issue #29 - Create book reviews table (#39)

--- database/main/tables/4_categories.sql
+++ database/main/tables/4_categories.sql
@@ -1,6 +1,6 @@
 create table if not exists main.categories(
-  id          smallserial         primary key,
-  name        text          not null,
+  id          smallserial  primary key,
+  name        text         not null,
   created_at  timestamptz  default now()
 );
 
