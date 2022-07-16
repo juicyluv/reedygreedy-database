79ff5c36c2205b3b4433bd166a895d77ed4caf45
juicyluv <juicyluv@mail.ru>
2022-07-16 23:19:37 +0300

    issue #20 - Add function to get book

--- database/main/tables/3_books.sql
+++ database/main/tables/3_books.sql
@@ -1,11 +1,11 @@
 create table if not exists main.books(
-  id          bigserial   primary key,
-  title       text        not null check(title != ''),
-  price       int         not null,
-  count       int         not null,
-  creator_id  bigint      not null references main.users(id),
+  id          bigserial     primary key,
+  title       text          not null check(title != ''),
+  price       decimal(10,2) not null,
+  count       int           not null,
+  creator_id  bigint        not null references main.users(id),
   description text,
-  created_at  timestamptz default now(),
+  created_at  timestamptz   default now(),
   updated_at  timestamptz
 );
 
--- patch/feed.sql
+++ patch/feed.sql
@@ -1,2 +1,2 @@
 insert into main.users(id, username, email, password)
-values (gen_random_uuid(), 'admin', 'admin@mail.com', 'admin');
+values (default, 'admin', 'admin@mail.com', 'admin');
