a26f89edaaabaaa967867a9f618209b181311eb6
Ilya <57060144+juicyluv@users.noreply.github.com>
2022-07-17 18:56:32 +0300

    issue #48 - Add creator_username out parameter in get_user and get_book functions (#54)

--- database/core/functions/get_book.sql
+++ database/core/functions/get_book.sql
@@ -1,13 +1,15 @@
-create or replace function core.get_book(_book_id    bigint,
-  out                                    title       text,
-  out                                    price       int,
-  out                                    count       decimal(10,2),
-  out                                    creator_id  bigint,
-  out                                    author_id   bigint,
-  out                                    description text,
-  out                                    created_at  timestamptz,
-  out                                    updated_at  timestamptz,
-  out                                    error       jsonb)
+create or replace function core.get_book(_book_id         bigint,
+  out                                    title            text,
+  out                                    price            int,
+  out                                    count            decimal(10,2),
+  out                                    creator_id       bigint,
+  out                                    creator_username text,
+  out                                    author_id        bigint,
+  out                                    author_name      text,
+  out                                    description      text,
+  out                                    created_at       timestamptz,
+  out                                    updated_at       timestamptz,
+  out                                    error            jsonb)
 as $$
 begin
 
@@ -16,7 +18,9 @@ begin
     b.price,
     b.count,
     b.creator_id,
+    u.username,
     b.author_id,
+    a.name,
     b.description,
     b.created_at,
     b.updated_at
@@ -25,12 +29,17 @@ begin
     price,
     count,
     creator_id,
+    creator_username,
     author_id,
+    author_name,
     description,
     created_at,
     updated_at
-  from
-    core.books b
+  from core.books b
+    left join core.users u
+      on u.id = b.creator_id
+    left join core.authors a
+      on a.id = b.author_id
   where b.id = _book_id;
 
   if not found then
