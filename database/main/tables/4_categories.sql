5f658c05a6793ac1ae72f38fc722a949331a0485
juicyluv <juicyluv@mail.ru>
2022-07-17 22:54:32 +0300

    issue #61 - Add function to update category

--- database/main/tables/4_categories.sql
+++ database/main/tables/4_categories.sql
@@ -1,7 +1,8 @@
 create table if not exists main.categories(
   id          smallserial  primary key,
   name        text         not null,
-  created_at  timestamptz  default now()
+  created_at  timestamptz  default now(),
+  updated_at  timestamptz
 );
 
 alter table main.categories owner to postgres;
