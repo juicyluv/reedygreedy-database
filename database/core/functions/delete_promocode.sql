6bc6b7ba44d71dd624e67e346789691ce6020bae
Ilya <57060144+juicyluv@users.noreply.github.com>
2022-07-19 02:17:57 +0300

    issue #98 - Add function to delete promocode (#115)

---
+++ database/core/functions/delete_promocode.sql
@@ -0,0 +1,47 @@
+create or replace function core.delete_promocode(_invoker_id   bigint,
+                                                 _promocode_id bigint)
+returns jsonb as $$
+begin
+
+  if not exists(select 1
+                from core.users u
+                where u.id = _invoker_id)
+  then
+    return jsonb_build_object(
+      'status', 1,
+      'details', jsonb_build_object(
+        'message', 'Invoker not found.',
+        'code', 'UNAUTHORIZED'
+      )
+    );
+  end if;
+
+  if not exists(select 1
+                from core.promocodes p
+                where p.id = _promocode_id)
+  then
+    return jsonb_build_object(
+      'status', 1,
+      'details', jsonb_build_object(
+        'message', 'Promocode not found.',
+        'code', 'NOT_FOUND'
+      )
+    );
+  end if;
+
+  delete from main.promocodes p
+  where p.id = _promocode_id;
+
+  return jsonb_build_object('status', 0);
+
+exception
+  when others then
+
+    return jsonb_build_object('status', -1);
+
+end;
+$$ language plpgsql volatile strict security definer;
+
+alter function core.delete_promocode(bigint, bigint) owner to postgres;
+grant execute on function core.delete_promocode(bigint, bigint) to postgres, web;
+revoke all on function core.delete_promocode(bigint, bigint) from public;
