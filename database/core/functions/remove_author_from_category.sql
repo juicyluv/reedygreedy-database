1e1848848ef21c6ae5e8c2371c5dacaec906aeb2
Ilya <57060144+juicyluv@users.noreply.github.com>
2022-07-18 16:03:28 +0300

    issue #68 - Add function to remove author from category (#88)

---
+++ database/core/functions/remove_author_from_category.sql
@@ -0,0 +1,75 @@
+create or replace function core.remove_author_from_category(_invoker_id  bigint,
+                                                            _author_id   bigint,
+                                                            _category_id smallint)
+returns jsonb as $$
+begin
+
+  if not exists(select 1
+                from core.users u
+                where u.id = _invoker_id)
+  then
+    return jsonb_build_object(
+      'status', 1,
+      'details', jsonb_build_object(
+        'message', 'Invoker not found.',
+        'code', 'UNAUTHORIZED'
+      )
+    );
+  end if;
+
+  if not exists(select 1
+                from core.authors a
+                where a.id = _author_id)
+  then
+    return jsonb_build_object(
+      'status', 1,
+      'details', jsonb_build_object(
+        'message', 'Author not found.',
+        'code', 'NOT_FOUND'
+      )
+    );
+  end if;
+
+  if not exists(select 1
+                from core.categories c
+                where c.id = _category_id)
+  then
+    return jsonb_build_object(
+      'status', 1,
+      'details', jsonb_build_object(
+        'message', 'Category not found.',
+        'code', 'NOT_FOUND'
+      )
+    );
+  end if;
+
+  if not exists(select 1
+                from core.authors_to_categories ac
+                where ac.author_id = _author_id
+                      and ac.category_id = _category_id)
+  then
+    return jsonb_build_object(
+      'status', 1,
+      'details', jsonb_build_object(
+        'message', 'Author is not in given category.',
+        'code', 'OBJECT_DEPENDENCY'
+      )
+    );
+  end if;
+
+  delete from main.authors_to_categories ac
+  where ac.author_id = _author_id and ac.category_id = _category_id;
+
+  return jsonb_build_object('status', 0);
+
+exception
+  when others then
+
+    return jsonb_build_object('status', -1);
+
+end
+$$ language plpgsql volatile security definer;
+
+alter function core.remove_author_from_category(bigint, bigint, smallint) owner to postgres;
+grant execute on function core.remove_author_from_category(bigint, bigint, smallint) to postgres, web;
+revoke all on function core.remove_author_from_category(bigint, bigint, smallint) from public;
