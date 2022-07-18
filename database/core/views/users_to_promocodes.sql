93e6c584e98097f61cd73ee6104dbe50e33d652d
juicyluv <juicyluv@mail.ru>
2022-07-19 02:47:23 +0300

    issue #102 - Add function to add promocode to user

--- database/core/views/users_to_promocodes.sql
+++ database/core/views/users_to_promocodes.sql
@@ -2,7 +2,8 @@ drop view if exists core.users_to_promocodes;
 create view core.users_to_promocodes as
 select
   user_id,
-  promocode_id
+  promocode_id,
+  created_at
 from main.users_to_promocodes;
 
 alter view core.users_to_promocodes owner to postgres;
--- database/main/tables/10_users_to_promocodes.sql
+++ database/main/tables/10_users_to_promocodes.sql
@@ -1,6 +1,7 @@
 create table if not exists main.users_to_promocodes(
   user_id      bigint not null references main.users(id),
-  promocode_id bigint not null references main.promocodes(id)
+  promocode_id bigint not null references main.promocodes(id),
+  created_at   timestamptz default now()
 );
 
 alter table main.users_to_promocodes owner to postgres;
