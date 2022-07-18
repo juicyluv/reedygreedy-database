29b2cb89613b78260e3adc1460c71749454a91cf
Ilya <57060144+juicyluv@users.noreply.github.com>
2022-07-18 18:33:50 +0300

    issue #93 - Add function to remove book review (#107)

---
+++ database/core/functions/remove_book_review.sql
@@ -0,0 +1,47 @@
+create or replace function core.remove_book_review(_invoker_id  bigint,
+                                                   _review_id   bigint)
+returns jsonb as $$
+begin
+
+  if not exists(select 1
+                from core.users u
+                where u.id = _invoker_id)
+  then
+    return jsonb_build_object(
+      'status', 1,
+      'details', jsonb_build_object(
+        'message', 'Invoker not found.',
+        'code', 'UNAUTHORIZED'
+      )
+    );
+  end if;
+
+  if not exists(select 1
+                from core.book_reviews br
+                where br.id = _review_id)
+  then
+    return jsonb_build_object(
+      'status', 1,
+      'details', jsonb_build_object(
+        'message', 'Review not found.',
+        'code', 'NOT_FOUND'
+      )
+    );
+  end if;
+
+  delete from main.book_reviews br
+  where br.id = _review_id;
+
+  return jsonb_build_object('status', 0);
+
+exception
+  when others then
+
+    return jsonb_build_object('status', -1);
+
+end;
+$$ language plpgsql volatile strict security definer;
+
+alter function core.remove_book_review(bigint, bigint) owner to postgres;
+grant execute on function core.remove_book_review(bigint, bigint) to postgres, web;
+revoke all on function core.remove_book_review(bigint, bigint) from public;
