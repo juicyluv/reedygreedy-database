0592769946642ed889b0c4e4e3d1cf76d4623bf0
Ilya <57060144+juicyluv@users.noreply.github.com>
2022-07-19 02:06:09 +0300

    issue #94 - Add function to list favourites (#111)

--- database/main/tables/8_favourites.sql
+++ database/main/tables/8_favourites.sql
@@ -1,6 +1,7 @@
 create table if not exists main.favourites(
-  user_id bigint not null references main.users(id),
-  book_id bigint not null references main.books(id)
+  user_id    bigint not null references main.users(id),
+  book_id    bigint not null references main.books(id),
+  created_at timestamptz default now()
 );
 
 alter table main.favourites owner to postgres;
